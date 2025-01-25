4b4814ec36e434126e890fb2d6f920a9507988fa
Shreyas Kulkarni <kshreyas93241@gmail.com>
2025-01-25 12:53:38 +0530

    Fifth Review

---
+++ FifthReview/AgeValid.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifthReview
+{
+    public class InvalidAgeException : Exception
+    {
+        public InvalidAgeException(string msg) : base(msg) { }
+    }
+    public class AgeValid
+    {
+        public static void Check()
+        {
+            Console.WriteLine("Enter age:");
+            int age = int.Parse(Console.ReadLine());
+            try
+            {
+                Validate(age);
+                Console.WriteLine("Valid age");
+            }
+            catch (InvalidAgeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public static void Validate(int age)
+        {
+            if(age < 18)
+            {
+                throw new InvalidAgeException("Age should be above 18");
+            }
+        }
+    }
+}
---
+++ FifthReview/PrimeCheck.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FifthReview
+{
+    public class PrimeCheck
+    {
+        [Required(ErrorMessage = "Number is required.")]
+        [Range(2, 1000, ErrorMessage = "Please enter a valid integer greater than or equal to 2.")]
+        public int Number { get; set; }
+        public static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
